2f9133851bdd561a4dddbf433eacfd1c46816a54
kelvinkrause <kelvinrkrause@outlook.com>
2024-10-20 10:47:42 -0300

    Clone para a IDE VSCode

---
+++ ScreenSound-04/Modelos/Musica.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace ScreenSound_04.Musica
+{
+    internal class Musica
+    {
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+        [JsonPropertyName("duration_ms")]
+        public int? Duracao { get; set; }
+        [JsonPropertyName("genre")]
+        public string? Genero { get; set; }
+
+
+        public void ExibeInformacoesMusica()
+        {
+            Console.WriteLine($"Artísta: {Artista}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Duração: {Duracao / 1000}");
+            Console.WriteLine($"Gênero: {Genero}");
+        }
+
+    }
+}
--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,14 +1,45 @@
-﻿using ScreenSound_04.Exercicio;
+﻿using System.Text.Json;
+using ScreenSound_04.Exercicio;
 using ScreenSound_04.Exercicio.IntegrandoUmaAPIExterna;
+using ScreenSound_04.Musica;
 
 public class Program
 {
+    
+    //https://hp-api.onrender.com/api/characters/house/gryffindor
+
     public static async Task Main(string[] args)
     {
+        await APIMusicas();
 
-        await new Exercicio1().API();
+        //await new Exercicio1().API();
         //new Exercicio2();
         //new Exercicio3();
         //new Exercicio4();
+
+    }
+
+    static async Task APIMusicas()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string response = await client
+                    .GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+                
+                //Deserialização da API de Musicas em uma Classe 
+                var musicas = JsonSerializer.Deserialize<List<Musica>>(response)!;
+                Console.WriteLine(musicas.Count);
+
+                musicas[0].ExibeInformacoesMusica();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERRO: " + ex.Message);
+            }
+        }
     }
 }
