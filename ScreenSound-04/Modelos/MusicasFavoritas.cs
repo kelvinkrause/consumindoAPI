9e4f5c0130c2e58a5e6a3059fa45651e5c612c08
kelvinkrause <kelvinrkrause@outlook.com>
2024-10-23 13:17:20 -0300

    Criando Arquivo com C# - Criado Classe MusicasFavoritas

---
+++ ScreenSound-04/Modelos/MusicasFavoritas.cs
@@ -0,0 +1,23 @@
+namespace ScreenSound_04.Modelos;
+public class MusicasFavoritas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListMusicasfavoritas { get; }
+
+    public MusicasFavoritas(string nome)
+    {
+        Nome = nome;
+        ListMusicasfavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListMusicasfavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Essas são as musicas favoritas -> {Nome}");
+        ListMusicasfavoritas.ForEach(musica => Console.WriteLine($"= {musica.Nome} de {musica.Artista}"));
+    }
+}
